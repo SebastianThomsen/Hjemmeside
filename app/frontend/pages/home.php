18bb0d1abd4c6cd236784cf40e1d43a18e43c353
KristofferHolm <kris136p@mail.hansenberg.dk>
2023-12-27 15:48:00 +0100

    Forside med billede, tekst, farver og links

--- app/frontend/pages/home.php
+++ app/frontend/pages/home.php
@@ -1,27 +1,27 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="homestyles.css">
-  </head>
-  <body>
-    <div class="header">
-      <div class="image-container">
-        <img src="livingroom.jpg" alt="Image 1" width="max" height="400">
-        <img src="livingroom2.jpg" alt="Image 2" width="max" height="400">
-      </div>
-      <div class="container">
-        <div class="break"></div> <!-- break -->
-  </div>
+</head>
+<body>
+<div class="header">
+        <img src="livingroom.jpg" alt="Electricity Image">
     </div>
-    <div class="discount-box green-box">
-       <p>Spar op til</p>
-      </div>
-
-      <div class="discount-box white-box">
-       <p>50%</p>
-      </div>
-  </body>
+    <div class="box-container">
+        <div class="white-box">
+            <p>Spar op til</p>
+        </div>
+        <div class="green-box">
+            <p>50%</p>
+        </div>
+    </div>
+    <div class="overlay">
+        <div class="text-container">
+            <p>Gå på opdagelse i den vilde elektriske verden</p>
+            <a href="#" class="rounded-button">Vis mere</a>
+        </div>
+    </div>
+</body>
 </html>
