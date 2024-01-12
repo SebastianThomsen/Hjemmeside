ccd91a87db07865ea72014e5f95b3f510e8a3f8c
Kristoffer <kfrankerholm@icloud.com>
2024-01-12 08:38:36 +0100

    kurv side

--- app/frontend/pages/about.php
+++ app/frontend/pages/about.php
@@ -7,16 +7,22 @@
     <title>Luksus EL - Om Os</title>
 </head>
 <body>
-
-    <div id="banner">
-        <h1>Om os</h1>
-    </div>
-
     <section id="omos">
-        <h2>Velkommen til Luksus El-Artikler</h2>
-        <p>Vi er dedikeret til at levere topkvalitets luksus el-artikler, der kombinerer elegant design og avanceret teknologi.</p>
-        <p>Vores team af eksperter arbejder hårdt for at sikre, at vores kunder får den bedste oplevelse med vores produkter.</p>
-        <p>Tag et kig på vores sortiment og oplev luksusen ved avancerede el-artikler.</p>
+        <h2>Om os</h2>
+        <p>
+Luksus EL™ er en virksomhed med base i Kolding, der specialiserer sig i salg af el-artikler af høj kvalitet. </br></br>
+
+Det hele startede i 2016, da "vi", to rejse- og vandsportsentusiaster, besluttede at samle vores idéer for at lade folk nyde fordelene af vores viden og eventyr! </br></br>
+
+Vores mission er at dele vores kærlighed til paddleboarding og rejser med vores fællesskab.</br>
+
+Du skal kunne tage din paddleboard med dig overalt og ikke kun i din lokale sø. Så tag den med dig overalt, verden er din legeplads!</br></br>
+
+Vi har distributionscentre i Europa og Nordamerika, og alle varer, der sælges på vores hjemmeside, er designet og konfigureret efter vores specifikationer.</br></br>
+
+HAR DU ET SPØRGSMÅL? Kontakt os venligst via vores kontakt side eller send os en e-mail på info@vacacicollection.com.</br></br>
+
+Vi har et fantastisk kundesupportteam, der arbejder 7 dage om ugen for at hjælpe dig med alle dine spørgsmål. Vi ønsker, at du skal nyde din shoppingoplevelse! Tak fordi du valgte os! Kontakt: info@vacacicollection.com</p>
     </section>
 
 </body>
---
+++ app/frontend/pages/kurv.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Luksus EL - Kurv</title>
+    <link rel="stylesheet" href="kurvstyles.css">
+</head>
+<body>
+    <section id="kurv">
+       
+       
+    </section>
+</body>
+</html>
