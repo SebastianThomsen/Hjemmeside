4714378353bd8042f50368b3ca7b74408dc671c8
Kristoffer <kfrankerholm@icloud.com>
2024-01-12 08:42:44 +0100

    Ændring i omkring os

--- app/frontend/pages/about.php
+++ app/frontend/pages/about.php
@@ -12,17 +12,15 @@
         <p>
 Luksus EL™ er en virksomhed med base i Kolding, der specialiserer sig i salg af el-artikler af høj kvalitet. </br></br>
 
-Det hele startede i 2016, da "vi", to rejse- og vandsportsentusiaster, besluttede at samle vores idéer for at lade folk nyde fordelene af vores viden og eventyr! </br></br>
+Det hele startede i 2023, da vi besluttede at der var mangel på kvalitets produkter inden for el branchen </br></br>
 
-Vores mission er at dele vores kærlighed til paddleboarding og rejser med vores fællesskab.</br>
+Vores mission er at dele vores kærlighed til el-arktikler</br></br>
 
-Du skal kunne tage din paddleboard med dig overalt og ikke kun i din lokale sø. Så tag den med dig overalt, verden er din legeplads!</br></br>
+Vi har distributionscentre i Europa, og alle varer der sælges på vores hjemmeside, er designet og konfigureret efter vores specifikationer.</br></br>
 
-Vi har distributionscentre i Europa og Nordamerika, og alle varer, der sælges på vores hjemmeside, er designet og konfigureret efter vores specifikationer.</br></br>
+HAR DU ET SPØRGSMÅL? Kontakt os venligst via vores kontakt side eller send os en e-mail på info@luksus-el.com.</br></br>
 
-HAR DU ET SPØRGSMÅL? Kontakt os venligst via vores kontakt side eller send os en e-mail på info@vacacicollection.com.</br></br>
-
-Vi har et fantastisk kundesupportteam, der arbejder 7 dage om ugen for at hjælpe dig med alle dine spørgsmål. Vi ønsker, at du skal nyde din shoppingoplevelse! Tak fordi du valgte os! Kontakt: info@vacacicollection.com</p>
+Vi har et fantastisk kundesupportteam, der arbejder 7 dage om ugen for at hjælpe dig med alle dine spørgsmål. Vi ønsker, at du skal nyde din shoppingoplevelse! Tak fordi du valgte os! Kontakt: info@luksus-el.com</p>
     </section>
 
 </body>
